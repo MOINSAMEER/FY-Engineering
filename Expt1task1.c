f67db8102d0b48ac8bf168485bd215c126c6d6cb
MOINSAMEER <mmoinmsameer7890@gmail.com>
2024-10-11 00:17:47 +0530

    Create Expt1task1.c

---
+++ Expt1task1.c
@@ -0,0 +1,29 @@
+/*Name: Moin M sameer bhurani
+UIN:241P059
+Title:WAP to print your name,age,class,division and UIN on the screen. Use escape sequences to properly format the output. Experiment 1, task 1
+Roll Number:53 */
+#include<stdio.h>
+
+
+int main()
+{
+  printf("Name\t\t:\tM Moin M Sameer Bhurani\n");
+  printf("Age\t\t\t:\t18\n");
+  printf("Class\t:\tFirst Year\n");
+  printf("Div\t\t\t:\tC3\n");
+  printf("UIN\t\t\t:\t241p059\n");
+
+
+  return 0;
+
+}
+
+/*Output:
+Name            :       M Moin M Sameer Bhurani
+Age                     :       18
+Class   :       First Year
+Div                     :       C3
+UIN                     :       241p059
+
+Process returned 0 (0x0)   execution time : 0.072 s
+Press any key to continue.*/
